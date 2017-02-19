f8f1d2df0cec5d1c3f9d064bc552d7b0f879282e
Dave Walk <daviddwalk@gmail.com>
2017-02-19 15:09:37 -0500

    Adding agent

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"net/http"
 
+	stackimpact "github.com/auto-profile/stackimpact-go"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	agent := stackimpact.NewAgent()
+	agent.Start(stackimpact.Options{
+		DashboardAddress: "http://localhost:8080",
+		AppName:          "test_server",
+	})
 	router := mux.NewRouter().StrictSlash(true)
 	v1 := router.PathPrefix("/agent/v1").Subrouter()
-	v1.HandleFunc("/", MetricHandler)
+	v1.HandleFunc("/{type}", MetricHandler)
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		fmt.Println(err)
