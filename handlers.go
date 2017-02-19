6071ac44238701e219bdeaf2cd928bb60bab3d56
Dave Walk <daviddwalk@gmail.com>
2017-02-19 15:09:52 -0500

    Printing out the request data (for now)

--- handlers.go
+++ handlers.go
@@ -1,7 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
 
 func MetricHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Printf("Type of request: %s\n", vars["type"])
+	reader, err := gzip.NewReader(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := io.Copy(os.Stdout, reader); err != nil {
+		panic(err)
+	}
 	return
 }
